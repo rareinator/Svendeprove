676dd9ee6dd4b8d105b56dfa453baaeeaa5aed0c
Simon Andersen <simon.andersen@pm.me>
2021-03-08 09:27:14 +0100

    Add basic structure for model training

--- Machine Learning/api/app.py
+++ Machine Learning/api/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, redirect
 from flask_restful import Api
 
 from resources.diagnosis import Diagnosis
-from common.data import DataCache
 
 app = Flask(__name__)
 api = Api(app)
