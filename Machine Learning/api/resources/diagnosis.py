8e6b67a7e0cf71ae7a3bd390264738e7b32cd8b8
Simon Andersen <simon.andersen@protonmail.com>
2021-03-15 15:33:26 +0100

    Change diagnosis POST method to use model prediction

--- Machine Learning/api/resources/diagnosis.py
+++ Machine Learning/api/resources/diagnosis.py
@@ -4,14 +4,21 @@ from flask_restful import Resource, reqparse
 from common.data import data
 from common.ml import PredictionService
 
-# Prediction = PredictionService('diagnosis')
+Prediction = PredictionService('diagnosis')
 
 class Diagnosis(Resource):
     def get(self):
         name = request.args.get('name', '')
+        symptoms = request.args.get('symptoms', '')
         diagnosis = data.diagnosis
+        s_dis = []
+        n_dis = []
         if name:
-            diagnosis = list(filter(lambda dis: name.lower() in dis.name.lower(), data.diagnosis))
+            n_dis = data.parse_diagnosis_name(name)
+        if symptoms:
+            s_dis = list(filter(lambda dis: any([dis_sym for dis_sym in dis.symptoms if sym.lower() in dis_sym.name.lower()] for sym in symptoms), data.diagnosis))
+        if name or symptoms:
+            diagnosis = n_dis.extend(s_dis)
             if not diagnosis:
                 return {
                     'code': 404,
@@ -28,20 +35,31 @@ class Diagnosis(Resource):
         parser.add_argument('symptoms', action='append')
         parser.add_argument('age', type=int)
         parser.add_argument('gender', choices=('m', 'f'))
+        parser.add_argument('bp', type=int, choices=(0, 1, 2)) #Low, normal, high
+        parser.add_argument('chol', type=int)
         parser = parser.parse_args()
         
-        symptoms = parser['symptoms']
+        symptoms = parser['symptoms'].split(",")
         age = parser['age']
         gender = parser['gender']
+        cholesterol = parser['chol']
+        b_pressure = parser['bp']
 
+        symptoms = [symptoms] if type(symptoms) is not list else symptoms
+        symptoms = [data.parse_symptom_name(sym).id for sym in symptoms]
 
-        diagnosis = filter(lambda dis: any([dis_sym for dis_sym in dis.symptoms if sym.lower() in dis_sym.name.lower()] for sym in symptoms), data.diagnosis)
-        if not diagnosis:
-            return {'code': 404,
-                    'message': "Not found"}, 404
+        prediction = PredictionService.predict([age, gender, cholesterol, b_pressure, symptoms])
+        diagnosis = list(filter(lambda dis: dis.id in prediction, data.diagnosis))
+        return {"code": 200,
+                "diagnosis": [{'name': dis.name} for dis in diagnosis]}
 
-        return {'code': 200,
-            'diagnosis': [{
-            'name': dis.name,
-            'symptoms': [sym.name for sym in dis.symptoms]
-        } for dis in diagnosis]}, 200
+        # diagnosis = filter(lambda dis: any([dis_sym for dis_sym in dis.symptoms if sym.lower() in dis_sym.name.lower()] for sym in symptoms), data.diagnosis)
+        # if not diagnosis:
+        #     return {'code': 404,
+        #             'message': "Not found"}, 404
+
+        # return {'code': 200,
+        #     'diagnosis': [{
+        #     'name': dis.name,
+        #     'symptoms': [sym.name for sym in dis.symptoms]
+        # } for dis in diagnosis]}, 200
