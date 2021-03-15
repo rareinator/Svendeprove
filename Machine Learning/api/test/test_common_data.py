3b2ba3539985befadb4826ee1b528699bb8e58aa
Simon Andersen <simon.andersen@protonmail.com>
2021-03-15 15:34:31 +0100

    Add more tests

--- Machine Learning/api/test/test_common_data.py
+++ Machine Learning/api/test/test_common_data.py
@@ -1,8 +1,15 @@
 import os
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 import tempfile
 import pytest
 
 from common.data import DataCache
+from common.ml import PredictionService
+
+IMG_SIZE = 96
 
 @pytest.fixture
 def data():
@@ -17,5 +24,33 @@ def test_symptoms_are_loaded(data):
 def test_diagnosis_symptom_relation(data):
     diag = next(filter(lambda dis: dis.name == "Abdominal trauma", data.diagnosis))
     assert len(diag.symptoms) == 2
-    
-    # def test_symptoms_are_loaded(self):
+
+def test_diagnosis_name_parse(data):
+    diagnosis = data.parse_diagnosis_name("inal tra")
+    assert diagnosis[0].name == "Abdominal trauma"
+
+def test_machine_learning_model_loaded(data):
+    Prediction = PredictionService("scan")
+    assert Prediction.model
+
+def test_scan_model_prediction_positive(data):
+    Prediction = PredictionService("scan")
+    img = _format_image("test_positive.jpg")
+    result = Prediction.predict(img)
+    assert result[1] * 100.0 > 50.0
+    assert result[0] * 100.0 < 50.0
+
+def test_scan_model_prediction_negative(data):
+    Prediction = PredictionService("scan")
+    img = _format_image("test_negative.jpg")
+    result = Prediction.predict(img)
+    assert result[0] * 100.0 > 50.0
+    assert result[1] * 100.0 < 50.0
+
+def _format_image(self, filepath):
+    img_array = cv2.imread(filepath, cv2.IMREAD_COLOR)
+    image = cv2.resize(img_array, (IMG_SIZE,IMG_SIZE))
+    image = np.reshape(img_array, (-1, IMG_SIZE,IMG_SIZE,3))
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    os.remove(filepath)
+    return image
--- Machine Learning/api/test/test_resource_diagnosis.py
+++ Machine Learning/api/test/test_resource_diagnosis.py
@@ -1,9 +1,9 @@
 import os
 import tempfile
+import base64
 
 import pytest
 
-from unittest import TestCase
 from app import app
 
 @pytest.fixture
@@ -11,15 +11,38 @@ def client():
     return app.test_client()
 
 
-def test_diagnosis_count(client):
-    response = client.get("/diagnosis")
-    data = response.json
-    assert len(data) > 10
-
 def test_diagnosis_partial_name(client):
     response = client.get("/diagnosis?name=abdominal")
     data = response.json
     assert len(data) == 6
+
+def test_diagnosis_get_all(client):
+    response = client.get("/diagnosis")
+    data = response.json
+    assert len(data) == 1347
+
+def test_get_diagnosis_by_symptom(client):
+    response = client.get("/diagnosis?symptoms=swell,pain")
+    data = response.json
+    assert len(data) > 0
+
+def test_scan_positive_result(client):
+    encoded_img = ""
+    with open("test_pos.jpg", "rb") as img_file:
+        encoded_img = base64.b64encode(img_file.read())
+    response = client.post("/scan", data=dict(scan=encoded_img))
+    data = response.json
+    assert data[1] * 100.0 > 50.0
+    assert data[0] * 100.0 < 50.0
+
+def test_scan_negative_result(client):
+    encoded_img = ""
+    with open("test_neg.jpg", "rb") as img_file:
+        encoded_img = base64.b64encode(img_file.read())
+    response = client.post("/scan", data=dict(scan=encoded_img))
+    data = response.json
+    assert data[0] * 100.0 > 50.0
+    assert data[1] * 100.0 < 50.0
     # def test_diagnosis_name_filter(self):
     #     assert
 
