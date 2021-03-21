da011f32bf4b93c6b81676fc2e26db595caf6df5
Nicolai Bjerg <looren@juel-kroyer.dk>
2021-03-21 12:16:31 +0100

    Fixed mispellings. Removed unnecessary comments.

--- Frontend/WebApp/DataAccessLibrary/IBookingData.cs
+++ Frontend/WebApp/DataAccessLibrary/IBookingData.cs
@@ -9,14 +9,14 @@ namespace DataAccessLibrary
     public interface IBookingData
     {
         /// <summary>
-        /// Sends a GET request to enpoint "/booking/byPatient/{param} to get bookings by the specified patient 
+        /// Sends a GET request to endpoint "/booking/byPatient/{param} to get bookings by the specified patient 
         /// </summary>
         /// <param name="patient"></param>
         /// <returns>List of patient booking objects</returns>
         Task<List<BookingModel>> GetBookingsByPatient(string patient);
 
         /// <summary>
-        /// Sends a GET request to enpoint "/booking/byEmployee/{param} to get bookings by the specified employee attending in the booking 
+        /// Sends a GET request to endpoint "/booking/byEmployee/{param} to get bookings by the specified employee attending in the booking 
         /// </summary>
         /// <param name="employee"></param>
         /// <returns>List of patient booking objects</returns>
@@ -43,27 +43,27 @@ namespace DataAccessLibrary
         Task<BookingModel> UpdateBooking(BookingModel booking);
 
         /// <summary>
-        /// Sends a GET request to enpoint "/admin/hospitals to get all hospitals
+        /// Sends a GET request to endpoint "/admin/hospitals to get all hospitals
         /// </summary>
         /// <returns>List of hospital objects</returns>
         Task<List<HospitalModel>> GetHospitals();
 
         /// <summary>
-        /// Sends a GET request to enpoint "/admin/doctors/inHospital/{param} to get available employees in specified hospital by id
+        /// Sends a GET request to endpoint "/admin/doctors/inHospital/{param} to get available employees in specified hospital by id
         /// </summary>
         /// <param name="hospitalId"></param>
         /// <returns>List of user objects</returns>
         Task<List<UserModel>> GetAvailableEmployees(int hospitalId);
 
         /// <summary>
-        /// Sends a POST request to enpoint "/booking/availableTimesForDoctor with {param} in the request body to get all available times for specified employee and day
+        /// Sends a POST request to endpoint "/booking/availableTimesForDoctor with {param} in the request body to get all available times for specified employee and day
         /// </summary>
         /// <param name="data"></param>
         /// <returns>List of datetimes</returns>
         Task<List<DateTime>> GetAvailableTimes(AvailableEmpDto data);
 
         /// <summary>
-        /// Sends a POST request to enpoint "/admin/availableBeds with {param} in the request body to get all available beds for specified hospitalId and timeframe
+        /// Sends a POST request to endpoint "/admin/availableBeds with {param} in the request body to get all available beds for specified hospitalId and timeframe
         /// </summary>
         /// <param name="data"></param>
         /// <returns>List of bed objects</returns>
